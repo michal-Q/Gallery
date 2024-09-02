55d9e9d0f49294ebf97f953b7226a215b3a1a5af
michal-Q <borowiak.michalq@gmail.com>
2024-09-02 16:44:17 +0200

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,243 @@
+﻿namespace Gallery
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button4 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.DeleteButton2 = new System.Windows.Forms.Button();
+            this.BackButton = new System.Windows.Forms.Button();
+            this.FullPictureBox = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.AddButton = new System.Windows.Forms.Button();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.panel1.SuspendLayout();
+            this.panel3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.FullPictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button4
+            // 
+            this.button4.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button4.BackColor = System.Drawing.Color.CornflowerBlue;
+            this.button4.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button4.FlatAppearance.BorderColor = System.Drawing.Color.CornflowerBlue;
+            this.button4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button4.Location = new System.Drawing.Point(1571, 0);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(30, 30);
+            this.button4.TabIndex = 0;
+            this.button4.Text = "X\r\n";
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button3
+            // 
+            this.button3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button3.BackColor = System.Drawing.Color.CornflowerBlue;
+            this.button3.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button3.FlatAppearance.BorderColor = System.Drawing.Color.CornflowerBlue;
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(1547, -1);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(28, 29);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "–";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.panel1.Controls.Add(this.panel3);
+            this.panel1.Controls.Add(this.textBox1);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.AddButton);
+            this.panel1.Controls.Add(this.panel2);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(800, 485);
+            this.panel1.TabIndex = 0;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.panel3.Controls.Add(this.label2);
+            this.panel3.Controls.Add(this.DeleteButton2);
+            this.panel3.Controls.Add(this.BackButton);
+            this.panel3.Controls.Add(this.FullPictureBox);
+            this.panel3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel3.Location = new System.Drawing.Point(0, 0);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(800, 485);
+            this.panel3.TabIndex = 7;
+            // 
+            // label2
+            // 
+            this.label2.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(240, 44);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(323, 23);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "label2";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // DeleteButton2
+            // 
+            this.DeleteButton2.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.DeleteButton2.FlatAppearance.BorderColor = System.Drawing.SystemColors.InactiveCaption;
+            this.DeleteButton2.FlatAppearance.MouseDownBackColor = System.Drawing.Color.White;
+            this.DeleteButton2.FlatAppearance.MouseOverBackColor = System.Drawing.Color.White;
+            this.DeleteButton2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.DeleteButton2.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.DeleteButton2.Location = new System.Drawing.Point(650, 26);
+            this.DeleteButton2.Name = "DeleteButton2";
+            this.DeleteButton2.Size = new System.Drawing.Size(122, 41);
+            this.DeleteButton2.TabIndex = 4;
+            this.DeleteButton2.Text = "Delete";
+            this.DeleteButton2.UseVisualStyleBackColor = true;
+            this.DeleteButton2.Click += new System.EventHandler(this.DeleteButton2_Click);
+            // 
+            // BackButton
+            // 
+            this.BackButton.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.BackButton.FlatAppearance.BorderColor = System.Drawing.SystemColors.InactiveCaption;
+            this.BackButton.FlatAppearance.MouseDownBackColor = System.Drawing.Color.White;
+            this.BackButton.FlatAppearance.MouseOverBackColor = System.Drawing.Color.White;
+            this.BackButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BackButton.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.BackButton.Location = new System.Drawing.Point(32, 26);
+            this.BackButton.Name = "BackButton";
+            this.BackButton.Size = new System.Drawing.Size(122, 41);
+            this.BackButton.TabIndex = 3;
+            this.BackButton.Text = "Back";
+            this.BackButton.UseVisualStyleBackColor = true;
+            this.BackButton.Click += new System.EventHandler(this.BackButton_Click);
+            // 
+            // FullPictureBox
+            // 
+            this.FullPictureBox.BackColor = System.Drawing.SystemColors.WindowFrame;
+            this.FullPictureBox.Location = new System.Drawing.Point(32, 85);
+            this.FullPictureBox.Name = "FullPictureBox";
+            this.FullPictureBox.Size = new System.Drawing.Size(740, 368);
+            this.FullPictureBox.TabIndex = 0;
+            this.FullPictureBox.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(61, 87);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(31, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "none";
+            // 
+            // AddButton
+            // 
+            this.AddButton.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.AddButton.FlatAppearance.BorderColor = System.Drawing.SystemColors.InactiveCaption;
+            this.AddButton.FlatAppearance.MouseDownBackColor = System.Drawing.Color.White;
+            this.AddButton.FlatAppearance.MouseOverBackColor = System.Drawing.Color.White;
+            this.AddButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.AddButton.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.AddButton.Location = new System.Drawing.Point(666, 42);
+            this.AddButton.Name = "AddButton";
+            this.AddButton.Size = new System.Drawing.Size(122, 41);
+            this.AddButton.TabIndex = 2;
+            this.AddButton.Text = "Add";
+            this.AddButton.UseVisualStyleBackColor = true;
+            this.AddButton.Click += new System.EventHandler(this.AddButton_Click);
+            // 
+            // panel2
+            // 
+            this.panel2.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.panel2.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.panel2.Location = new System.Drawing.Point(13, 103);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(776, 341);
+            this.panel2.TabIndex = 5;
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox1.Location = new System.Drawing.Point(2, 469);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(490, 13);
+            this.textBox1.TabIndex = 6;
+            this.textBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.BackColor = System.Drawing.Color.CornflowerBlue;
+            this.ClientSize = new System.Drawing.Size(800, 485);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimumSize = new System.Drawing.Size(0, 485);
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel3.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.FullPictureBox)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button AddButton;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Button DeleteButton2;
+        private System.Windows.Forms.Button BackButton;
+        private System.Windows.Forms.PictureBox FullPictureBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,335 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Gallery
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            CheckFolder();
+            BrowseForImages();
+            panel3.Visible = false;
+            textBox1.Text = subFolderPath;
+        }
+
+        private const int cGrip = 16;
+        private const int cCaption = 32;
+        protected override void WndProc(ref Message m)
+        {
+            if (m.Msg == 0x84)
+            {
+                Point pos = new Point(m.LParam.ToInt32());
+                pos = this.PointToClient(pos);
+                if (pos.Y < cCaption)
+                {
+                    m.Result = (IntPtr)2;
+                    return;
+                }
+
+                if (pos.X >= this.ClientSize.Width - cGrip && pos.Y >= this.ClientSize.Height - cGrip)
+                {
+                    m.Result = (IntPtr)17;
+                    return;
+                }
+            }
+            base.WndProc(ref m);
+        }
+
+        List<string> FoundImages;
+        string subFolderPath;
+        bool subFolderbool = false;
+        List<PictureBox> pictureBoxList = new List<PictureBox>();
+        string GlobalImagePath = "none";
+        PictureBox GlobalPictureBox;
+
+        private void CheckFolder()
+        {
+            string appDirectory = AppDomain.CurrentDomain.BaseDirectory;
+            string subFolderName = "ImageGallery";
+
+            subFolderPath = Path.Combine(appDirectory, subFolderName);
+
+            if (!Directory.Exists(subFolderPath))
+            {
+                Directory.CreateDirectory(subFolderPath);
+                Console.WriteLine($"Folder '{subFolderName}' został utworzony w {subFolderPath}");
+                subFolderbool = true;
+            }
+            else
+            {
+                Console.WriteLine($"Folder '{subFolderName}' już istnieje w {subFolderPath}");
+                subFolderbool = true;
+            }
+        }
+
+        private void BrowseForImages()
+        {
+            if (subFolderbool)
+            {
+                label1.Text = "Forlder found 1";
+                FoundImages = Directory.GetFiles(subFolderPath, "*.*")
+                    .Where(file => file.ToLower().EndsWith("jpg") || file.ToLower().EndsWith("gif")
+                                 || file.ToLower().EndsWith("png") || file.ToLower().EndsWith("bmp"))
+                    .ToList();
+                label1.Text = "Forlder found 2";
+                LoadImages();
+            }
+            else
+            {
+                label1.Text = "Forlder not found";
+            }
+        }
+
+        
+
+        private void LoadImages()
+        {
+            if (FoundImages.Count > 0)
+            {
+                Panel imagePanel = panel2;
+                imagePanel.AutoScroll = true;
+                imagePanel.VerticalScroll.Visible = false;
+                imagePanel.HorizontalScroll.Visible = false;
+
+                foreach(PictureBox Oldimagebox in imagePanel.Controls)
+                {
+                    imagePanel.Controls.Remove(Oldimagebox);
+                    pictureBoxList.Remove(Oldimagebox);
+                    pictureBoxList.Clear();
+                }
+
+                int counter = 0;
+                int x = 50;
+                int y = 10;
+                int margin = 15;
+
+                foreach (string imagePath in FoundImages)
+                {
+                    PictureBox imagebox = new PictureBox();
+                    imagebox.SizeMode = PictureBoxSizeMode.CenterImage;
+                    imagebox.Size = new Size(155, 155);
+                    imagebox.Location = new Point(x, y);
+                    imagebox.Paint += (s, e) => PaintPictureBox(s, e, imagePath);
+
+                    pictureBoxList.Add(imagebox);
+                    imagePanel.Controls.Add(imagebox);
+
+                    x += imagebox.Width + margin;
+                    if (x + imagebox.Width + margin > imagePanel.Width)
+                    {
+                        x = 50;
+                        y += imagebox.Height + margin;
+                    }
+
+                    counter++;
+                    imagebox.BringToFront();
+                    
+                    imagebox.Tag = imagePath;
+                    imagebox.Click += FormMouseDown;
+                }
+
+                label1.Text = $"{counter} images loaded";
+            }
+            else
+            {
+                label1.Text = "No images found";
+            }
+        }
+
+        private void PaintPictureBox(object sender, PaintEventArgs e, string imagePath)
+        {
+            PictureBox pb = sender as PictureBox;
+            if (pb != null)
+            {
+                if (!File.Exists(imagePath))
+                {
+                    panel2.Controls.Remove(pb);
+                    pictureBoxList.Remove(pb);
+                    pb.Dispose();
+                    UpdatePictureBoxLocation();
+                    return;
+                }
+
+                Image img = Image.FromFile(imagePath);
+                Rectangle srcRect;
+                Rectangle destRect = new Rectangle(0, 0, pb.Width, pb.Height);
+
+                float imgAspect = (float)img.Width / img.Height;
+                float pbAspect = (float)pb.Width / pb.Height;
+
+                if (imgAspect > pbAspect)
+                {
+                    int scaledWidth = (int)(img.Height * pbAspect);
+                    srcRect = new Rectangle((img.Width - scaledWidth) / 2, 0, scaledWidth, img.Height);
+                }
+                else
+                {
+                    int scaledHeight = (int)(img.Width / pbAspect);
+                    srcRect = new Rectangle(0, (img.Height - scaledHeight) / 2, img.Width, scaledHeight);
+                }
+
+                e.Graphics.DrawImage(img, destRect, srcRect, GraphicsUnit.Pixel);
+                img.Dispose();
+            }
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
+        }
+
+        public void FormMouseDown(object sender, EventArgs e)
+        {
+            PictureBox currentBox = (PictureBox)sender;
+            string SelectedImagePath = currentBox.Tag.ToString();
+
+            if (!File.Exists(SelectedImagePath))
+            {
+                panel2.Controls.Remove(currentBox);
+                pictureBoxList.Remove(currentBox);
+                currentBox.Dispose();
+                UpdatePictureBoxLocation();
+                return;
+            }
+
+            ShowFullImage(SelectedImagePath);
+            GlobalPictureBox = currentBox;
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ShowFullImage(string SelectedImagePath)
+        {
+            DisposeCurrentImage();
+            FullPictureBox.Image = Image.FromFile(SelectedImagePath);
+            FullPictureBox.SizeMode = PictureBoxSizeMode.Zoom;
+            panel3.Visible = true;
+            GlobalImagePath = SelectedImagePath;
+            string fileName = Path.GetFileName(SelectedImagePath);
+            label2.Text = fileName;
+        }
+
+        private void BackButton_Click(object sender, EventArgs e)
+        {
+            panel3.Visible = false;
+            DisposeCurrentImage();
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void DisposeCurrentImage()
+        {
+            if (FullPictureBox.Image != null)
+            {
+                FullPictureBox.Image.Dispose();
+                FullPictureBox.Image = null;
+            }
+        }
+
+        private void DeleteButton2_Click(object sender, EventArgs e)
+        {
+            pictureBoxList.Remove(GlobalPictureBox);
+            GlobalPictureBox.Dispose();
+            DisposeCurrentImage();
+            File.Delete(GlobalImagePath);
+            panel3.Visible = false;
+            UpdatePictureBoxLocation();
+        }
+
+        private void UpdatePictureBoxLocation()
+        {
+            int counter = 0;
+            int x = 50;
+            int y = 10;
+            int margin = 15;
+
+            foreach (PictureBox imagebox in pictureBoxList)
+            {
+
+                imagebox.Location = new Point(x, y - panel2.VerticalScroll.Value);
+                x += imagebox.Width + margin;
+                if (x + imagebox.Width + margin > panel2.Width)
+                {
+                    x = 50;
+                    y += imagebox.Height + margin;
+                }
+                counter++;
+            }
+            label1.Text = $"{counter} images loaded.";
+        }
+
+        private void AddButton_Click(object sender, EventArgs e)
+        {
+            AddingImage();
+            UpdatePictureBoxLocation();
+        }
+
+        private void AddingImage()
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Multiselect = true;
+            openFileDialog.Filter = "Image Files(*.PNG;*.JPG;*.GIF)|*.PNG;*.JPG;*.GIF";
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                int x = 50;
+                int y = 10;
+                int margin = 15;
+
+                foreach(string imagePath in openFileDialog.FileNames)
+                {
+                    //PictureBox imagebox = new PictureBox();
+                    //imagebox.SizeMode = PictureBoxSizeMode.CenterImage;
+                    //imagebox.Size = new Size(155, 155);
+                    //imagebox.Location = new Point(x, y);
+                    //
+                    //
+                    //pictureBoxList.Add(imagebox);
+                    //panel2.Controls.Add(imagebox);
+                    //
+                    //x += imagebox.Width + margin;
+                    //if (x + imagebox.Width + margin > panel2.Width)
+                    //{
+                    //    x = 50;
+                    //    y += imagebox.Height + margin;
+                    //}
+
+                    string fileName = Path.GetFileName(imagePath);
+                    string savePath = Path.Combine(subFolderPath, fileName);
+                    Image img = Image.FromFile(imagePath);
+                    img.Save(savePath);
+                    img.Dispose();
+
+                    //imagebox.Paint += (s, e) => PaintPictureBox(s, e, savePath);
+                    //imagebox.BringToFront();
+                    //imagebox.Tag = savePath;
+                    //imagebox.Click += FormMouseDown;
+                }
+                BrowseForImages();
+            }
+        }
+    }
+}
